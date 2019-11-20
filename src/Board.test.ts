294ea3dddfffbc944eb7848a5d1a47b457e5eb11
Koki Maruyama <kokitechno@gmail.com>
2019-11-20 10:14:11 +0900

    テストを修正

--- src/Board.test.ts
+++ src/Board.test.ts
@@ -3,10 +3,9 @@ import Board, { Disk } from './Board'
 it('create a board which is made up of 64 squares arranged in 8 rows and 8 columns', () => {
   const board = new Board()
 
-  expect(Board.SQUARES_PER_LINE).toBe(8)
-  expect(board.rows).toBe(Board.SQUARES_PER_LINE)
-  expect(board.columns).toBe(Board.SQUARES_PER_LINE)
-  expect(board.squares).toBe(Board.SQUARES_PER_LINE ** 2)
+  expect(board.rows).toBe(8)
+  expect(board.columns).toBe(8)
+  expect(board.squares).toBe(64)
 })
 
 it('create a initial state board', () => {
